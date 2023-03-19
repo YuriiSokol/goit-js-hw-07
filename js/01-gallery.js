c23a143924c7b8d361beca089fc40508c710ae37
YuriiSokol <yura357117@gmail.com>
2023-03-19 13:22:40 +0100

    a

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+// building html structure
+galleryItems.forEach((item) => {
+  let element = document.createElement("div");
+  element.classList.add("gallery__item");
+
+  let link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  let img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = item.preview;
+  img.alt = item.description;
+  img.setAttribute("data-source", item.original);
+  link.append(img);
+  element.append(link);
+  gallery.append(element);
+});
+
+//Click event listener
+
+gallery.addEventListener("click", (event) => {
+  //finding big image url
+  event.preventDefault();
+
+  if (event.target.classList.value !== "gallery__image") {
+    return;
+  }
+
+  let findGalleryItemByPreview = galleryItems.find(
+    (el) => el.preview === event.target.src
+  );
+
+  // using balicLightBox to create div
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${findGalleryItemByPreview.description}" width="800" height="600">
+`);
+
+  instance.show();
+
+  // modal close from key Escape
+
+  const close = document.addEventListener(
+    "keydown",
+    (event) => {
+      if (event.key === "Escape") {
+        instance.close();
+        document.removeEventListener(Object, close);
+      }
+    },
+    { once: true }
+  );
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+galleryItems.forEach((item) => {
+  let link = document.createElement("a");
+  link.classList.add("gallery__item");
+  link.href = item.original;
 
-console.log(galleryItems);
+  let img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = item.preview;
+  img.alt = item.description;
+
+  link.append(img);
+  gallery.append(link);
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
